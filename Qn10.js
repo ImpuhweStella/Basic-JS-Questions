303a0c2696f92c1b387313a7b6c9bd12c2e32a06
ImpuhweStella <stellahimpuhwe346@gmail.com>
2023-11-24 04:45:22 +0200

    second commit

---
+++ Qn10.js
@@ -0,0 +1,12 @@
+function patternPrinter(n){
+    
+    for (let j = 0; j < n ; j++) {
+        var arr=[];
+        for (let i = 0; i <= n; i++) {
+            arr+="* ";
+            
+        }
+        console.log(arr);
+    }
+}
+patternPrinter(4);
